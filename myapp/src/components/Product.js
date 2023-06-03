3faa9a1a3a6d20fdbcb4763627abadf0ff21b5fa
kiShubham <shubhamshubhank1997@gmail.com>
2023-06-03 14:26:39 +0530

    dependcies installed

---
+++ myapp/src/components/Product.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Product = () => {
+  const [productList, setProductList] = useState([]);
+
+  const fetchProduct = async () => {
+    let res = await axios(
+      "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
+    );
+    if (res && res.data) {
+      setProductList(res.data);
+    }
+  };
+  useEffect(() => {
+    const onload = async () => {
+      let ers = await fetchProduct();
+    };
+    onload();
+  }, []);
+
+  return <div>Product</div>;
+};
+
+export default Product;
